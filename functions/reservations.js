095b6260709ce3060993f21dd51f426e4959945d
Joel Eskelsen <joel@tsicloud.com>
2025-04-17 00:27:59 -0600

    initial reservations.js

---
+++ functions/reservations.js
@@ -0,0 +1,14 @@
+export async function onRequestGet(context) {
+    const { env } = context;
+    const { results } = await env.DB.prepare(
+      `SELECT r.id, r.start_time, r.end_time, r.flight_review,
+              u.name AS user_name, a.tail_number AS airplane_tail
+       FROM reservations r
+       JOIN users u ON r.user_id = u.id
+       JOIN airplanes a ON r.airplane_id = a.id
+       WHERE r.start_time >= datetime('now')
+       ORDER BY r.start_time ASC`
+    ).all();
+  
+    return Response.json(results);
+  }
