d2eec62a83a74858081b7006322e4dc48acfd9e9
Joel Eskelsen <joel@tsicloud.com>
2025-04-17 17:45:49 -0600

    calendar in app.js update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function CalendarPage() {
-  const [calendarView] = useState('timeGridWeek');
-
   return (
     <div>
       {/* Calendar rendering logic */}
